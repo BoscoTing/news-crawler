4890cc065dfeffd5cf51e4cd255ad3e25fbbb1eb
Hao Ting <boscoting0522@gmail.com>
2024-11-18 22:09:09 +0800

    chore: Add settings

---
+++ src/scraper/config/config.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Config(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env", 
+        env_ignore_empty=True, 
+        extra="ignore",
+    )
+    UDN_SITEMAP_URL: str
+    START_DATE: datetime = datetime.strptime("2024-01-01", "%Y-%m-%d")
+    END_DATE: datetime = datetime.strptime("2024-01-31", "%Y-%m-%d")
+
+    S3_BUCKET_NAME: str
+    S3_BASE_PATH: str
+    S3_REGION_NAME: str
+
+    S3_ACCESS_KEY_ID: str
+    S3_SECRET_ACCESS_KEY: str
+
+settings = Config()
